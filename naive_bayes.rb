205be10a3d7e14fb6f46a3d06a27234ab0399298
John <ubuntu@ubuntu.local>
2013-02-21 21:42:41 -0600

    Refactor and complete.

--- naive_bayes.rb
+++ naive_bayes.rb
@@ -1,15 +1,3 @@
-class Person < Struct.new(:height, :weight, :foot_size); end
-
-class Male < Person; end
-
-class Female < Person; end
-
-males = []
-males << Male.new(6, 180, 12) << Male.new(5.92, 190, 11) << Male.new(5.58, 170, 12) << Male.new(5.92, 165, 10)
-
-females = []
-females << Female.new(5, 100, 6) << Female.new(5.5, 150, 8) << Female.new(5.42, 130, 7) << Female.new(5.75, 150, 9)
-
 module Enumerable
   def sum
     self.inject(0){|accum, i| accum + i }
@@ -42,20 +30,35 @@ def probability(mean, variance, value)
   (1 / Math.sqrt(2 * Math::PI * variance)) * Math.exp((-1 * (value - mean) ** 2)/ (2 * variance)) 
 end
 
+def total_probability(unknown_sample, group)
+  probabilities = Sample.members.map do |attr|
+    attr_mean = mean(group, attr)
+    attr_variance = sample_variance(group, attr)
 
-male_probabilities = [[:height, 6], [:weight, 130], [:foot_size, 8]].map do |key, value|
-  male_attr_mean = mean(males, key)
-  male_attr_variance = sample_variance(males, key)
+    prob = probability(attr_mean, attr_variance, unknown_sample[attr])
+    # puts "prob #{attr}: #{prob}"
+    prob
+  end
+  
+  probabilities << 0.5
 
-  #puts "male #{key} mean: #{male_attr_mean}"
-  #puts "male #{key} variance: #{male_attr_variance}"
-  prob = probability(male_attr_mean, male_attr_variance, value)
-  puts "prob #{key}: #{prob}"
-  prob
+  probabilities_product = probabilities.inject(1){|accum, i| accum * i }
 end
 
-male_probabilities << 0.5
+class Sample < Struct.new(:height, :weight, :foot_size); end
 
-male_probabilities_product = male_probabilities.inject(1){|accum, i| accum * i }
+males = []
+males << Sample.new(6, 180, 12) << Sample.new(5.92, 190, 11) << Sample.new(5.58, 170, 12) << Sample.new(5.92, 165, 10)
+
+females = []
+females << Sample.new(5, 100, 6) << Sample.new(5.5, 150, 8) << Sample.new(5.42, 130, 7) << Sample.new(5.75, 150, 9)
+
+unknown = Sample.new(6, 130, 8)
+
+male_probabilities_product = total_probability(unknown, males)
+female_probabilities_product = total_probability(unknown, females)
 
 puts "male_probabilities_product: #{male_probabilities_product}"  
+puts "female_probabilities_product: #{female_probabilities_product}"  
+
+puts male_probabilities_product > female_probabilities_product ? "This person is likely male" : "This person is likely female" 
